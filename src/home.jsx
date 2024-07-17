804ff901d197c3bd72e66943b6331e5b564510e5
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-17 15:48:00 +0200

    Added input

--- src/home.jsx
+++ src/home.jsx
@@ -5,15 +5,21 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 const Home = () => {
   return (
     <>
+      <div className='w-1/2 m-auto flex flex-col justify-center h-screen'>
         <div className="navbar">
-            <h1 className='text-center bg-red-300 font-semibold text-4xl'>Todo App</h1>
-        </div>
+              <h1 className='text-center bg-red-300 font-semibold text-4xl'>Todo Application</h1>
+          </div>
 
-        <div className='bg-gray-200'>
-          <input type="text" placeholder='Add a todo'/>
-          <button id='add'>Add</button>
-          <button>< FontAwesomeIcon icon={faTrash} id='delete'/></button>
-        </div>
+          <div className='bg-gray-200'>
+
+            <div className='flex justify-center gap-10'>
+              <input type="text" placeholder='Add a todo' className='w-1/2 focus:outline-none'/>
+              <button id='add' className='border border-black px-4 py-2'>Add</button>
+              <button>< FontAwesomeIcon icon={faTrash} id='delete' className='text-red-400'/></button>
+            </div>
+          </div>
+      </div>
+      
     
     </>
   )
