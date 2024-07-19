fbf00e626d9cd90186b108865e0202e8bfa76684
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-19 09:27:09 +0200

    Added unordered list

--- src/navigation.jsx
+++ src/navigation.jsx
@@ -6,17 +6,17 @@ const Navigation = () => {
         <div className="bg-gray-800 w-[60%] m-auto text-white items-center h-screen my-56 rounded-xl">
           <div className='flex items-center justify-between px-8 py-10 bg-neutral-800'>
             <nav className="flex gap-4">
-            <img src={require('./reactjs-icon.png')} alt="" />
-              <h1 className='text-3xl font-bold'>ReactFacts</h1>
+            <img src={require('./reactjs-icon.png')} alt="" className='w-9'/>
+              <h1 className='text-3xl font-bold text-cyan-300'>ReactFacts</h1>
             </nav>
 
-            <h2>React-Course - Project1</h2>
+            <h2 className='text-2xl font-bold'>React-Course - Project1</h2>
           </div>
           
           
           <div className="body1">
-            <h1>Fun facts about React</h1>
-            <ul className="body">
+            <h1 className='text-5xl pl-8'>Fun facts about React</h1>
+            <ul className="">
               <li>Was first released in 2013</li>
               
               <li>Was originally created by Jordan Walke</li>
