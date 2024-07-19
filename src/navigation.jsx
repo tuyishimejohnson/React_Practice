300c47aa34777824424615ee5fbb73b5c204d35f
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-19 11:54:08 +0200

    Added a marker and bullet points

--- src/navigation.jsx
+++ src/navigation.jsx
@@ -4,7 +4,7 @@ const Navigation = () => {
   return (
     <>
         <div className="bg-gray-800 w-[60%] m-auto text-white items-center h-screen my-56 rounded-xl">
-          <div className='flex items-center justify-between px-8 py-10 bg-neutral-800'>
+          <div className='flex items-center justify-between px-8 py-10 bg-neutral-800 rounded-t-xl'>
             <nav className="flex gap-4">
             <img src={require('./reactjs-icon.png')} alt="" className='w-9'/>
               <h1 className='text-3xl font-bold text-cyan-300'>ReactFacts</h1>
@@ -16,7 +16,7 @@ const Navigation = () => {
           
           <div className="pt-16">
             <h1 className='text-5xl pl-8 pb-9'>Fun facts about React</h1>
-            <ul className="pl-16 flex flex-col gap-3 text-xl">
+            <ul className="pl-16 flex flex-col gap-3 text-xl list-disc marker:text-cyan-300 marker:text-2xl">
               <li>Was first released in 2013</li>
               <li>Was originally created by Jordan Walke</li>
               <li>Has well over 100K stars on GitHub</li>
