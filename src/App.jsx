6a38ec6ca44f1b245d298ca12f015461e0b5ad14
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-19 09:42:27 +0200

    Added unordered list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import "./index.css";
 
+
 import Navigation from "./navigation.jsx";
 
 function App() {
--- src/navigation.jsx
+++ src/navigation.jsx
@@ -16,15 +16,11 @@ const Navigation = () => {
           
           <div className="body1">
             <h1 className='text-5xl pl-8'>Fun facts about React</h1>
-            <ul className="">
+            <ul className="pl-16">
               <li>Was first released in 2013</li>
-              
               <li>Was originally created by Jordan Walke</li>
-              
               <li>Has well over 100K stars on GitHub</li>
-              
               <li>Is maintained by Meta</li>
-              
               <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
           </div>
