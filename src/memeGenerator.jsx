30192cd2f4cca9b06d2f52234843bdcb01a4e47c
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-25 08:45:05 +0200

    Random meme images

--- src/memeGenerator.jsx
+++ src/memeGenerator.jsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react'
 import memeData from './memeData'
 
-const MemeGenerator = () => {
-    
+const MemeGenerator = () => {    
     let arrayData = memeData.data.memes
     const urls = arrayData.map(item => item.url)
-    const [memeImage, setMemeImage] = useState("")
+    /* const [memeImage, setMemeImage] = useState("") */
+
+    
+
+
+    const [meme, setMeme] = useState({
+        topText:"",
+        bottomText:"",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+
+    const [allMemeImages, setAllMemeImages] = useState(urls)
 
     const getUrls = () => {
-        const randomIndex = Math.floor(Math.random() * urls.length)
-        let item = urls[randomIndex]
-        setMemeImage(item)
+        const randomIndex = Math.floor(Math.random() * allMemeImages.length)
+        let item = allMemeImages[randomIndex]
+        console.log(item)
+        setMeme(prevState => ({
+            ...prevState,
+            randomImage: item 
+
+        }))
 }   
+
   return (
     <>
         <div className='w-[40%] m-auto mt-36 h-80'>
-            <div className='flex bg-gradient-to-r from-violet-700 to-purple-500 text-white items-center justify-between px-6 py-3'>
+            <div className='flex bg-gradient-to-r from-violet-700 to-purple-500
+             text-white items-center justify-between px-6 py-3'>
                 <div className='flex items-center gap-3'>
                     <img src={require("./images/Troll Face.png")} alt="" className='w-16'/>
                     <h1 className='text-2xl font-semibold'>Meme Generator</h1>
@@ -36,11 +54,10 @@ const MemeGenerator = () => {
                     <input type="text" placeholder='Second text' className='border border-black w-full pl-3 py-3 rounded-md'/>
                 </div>
             </div>
-            
             <button className='bg-gradient-to-r from-violet-700 to-purple-500
              text-white px-5 py-2 w-full rounded-md' onClick={getUrls}>Get a new meme image  🖼</button>
 
-             <img src={memeImage} alt="images" className='m-auto px-7 py-10'/>
+             <img src={meme.randomImage} alt="images" className='m-auto px-7 py-10'/>
         </div>
     </>
   )
