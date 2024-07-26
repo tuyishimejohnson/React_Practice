784ecd70470468354dc1d7f7eb882b0f7a54d145
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-26 17:44:19 +0200

    Submitting a form

--- src/forms.jsx
+++ src/forms.jsx
@@ -2,7 +2,13 @@ import React, { useState } from 'react'
 
 const Forms = () => {
 
-    const [name, setName] = useState({firstName:"", lastName:"", email:"", comments:""})
+    const [name, setName] = useState({firstName:"", 
+                                    lastName:"", 
+                                    email:"", 
+                                    comments:"",
+                                    isChecked: true,
+                                    favoriteColor: ""
+                                })
 
     const handleInput = (event) => {
         setName(prevState => ({
@@ -11,11 +17,16 @@ const Forms = () => {
         }))
     }
 
-    console.log(name)
+    const handleSubmit = (event) => {
+        console.log("Shiii!")
+    }
  
   return (
     <div className='w-[40%] m-auto my-10'>
         <h1>Use of forms in React</h1>
+
+
+        <form onSubmit={handlSubmit}>
 
         <input type="text" name="firstName"  placeholder='First name' onChange={handleInput} value={name.firstName}/>
         <input type="text" name="lastName" placeholder='Last name' onChange={handleInput} value={name.lastName}/>
@@ -26,6 +37,22 @@ const Forms = () => {
             value={name.comments}
             onChange={handleInput}
         />
+
+        <input type="checkbox" checked={name.isChecked} id='isChecked'/>
+        <label htmlFor="isChecked">Is it Y?</label>
+        <select name="favoriteColor" id="" value={name.favoriteColor} onChange={handleInput}>
+            <option value="red">-- Choose --</option>
+            <option value="red">Red</option>
+            <option value="green">green</option>
+            <option value="blue">blue</option>
+            <option value="white">White</option>
+        </select>
+        <br />
+
+        <button>Submit</button>
+        </form>
+
+        
     </div>
   )
 }
